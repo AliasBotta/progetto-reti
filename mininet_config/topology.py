a13e880e66e2c7d8dea156f0b46c9265073498bf
Domenico Puglisi <domenico.puglisi@community.unipa.it>
2025-01-11 11:45:59 +0100

    Configurazione iniziale per la topologia del progetto.
    Rimosso type hint dal dizionario `topos` per motivi di compatibilità
    con Python 3.9.
    Aggiunta maschera di rete in notazione CIDR all'indirizzo IP di ciascun host.

--- mininet_config/topology.py
+++ mininet_config/topology.py
@@ -11,9 +11,37 @@ class TestTopology(Topo):
         self.addLink(s1, h2)
 
 
+class ProjectTopology(Topo):
+    """Topologia assegnata per il progetto da realizzare."""
+
+    def build(self):
+        # Subnet 1
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+        sw1 = self.addSwitch('sw1')
+        for host in (h1, h2):
+            self.addLink(host, sw1, bw=100, delay='0.05ms')
+
+        # Subnet 2
+        h3 = self.addHost('h3', ip='11.0.0.1/24')
+        sw2 = self.addSwitch('sw2')
+        self.addLink(h3, sw2, bw=1, delay='0.5ms')
+
+        # Subnet 3
+        h4 = self.addHost('h4', ip='192.168.1.1/24')
+        sw3 = self.addSwitch('sw3')
+        self.addLink(h4, sw3, bw=100, delay='0.05ms')
+
+        # Subnet 4
+        h5 = self.addHost('h5', ip='10.8.1.1/24')
+        sw4 = self.addSwitch('sw4')
+        self.addLink(h5, sw4, bw=100, delay='0.05ms')
+    
+
 # Mininet si aspetta che sia presente un dizionario `topos`
 # nello scope globale del proprio file di configurazione,
 # contenente tutte le topologie custom che si desidera utilizzare.
-topos: dict[str, Topo] = {
+topos = {
     "test": TestTopology,
+    "project": ProjectTopology,
 }
