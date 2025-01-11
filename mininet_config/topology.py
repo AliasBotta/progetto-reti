246386bbb11d7efcd1e42d76a72d7133c7a478e9
Domenico Puglisi <domenico.puglisi@community.unipa.it>
2025-01-11 15:44:38 +0100

    Assegnati indirizzi IP alle interfacce di ciascuno switch tramite controller di rete.

--- mininet_config/topology.py
+++ mininet_config/topology.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, field
+from typing import List, Dict
+
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSKernelSwitch
 from mininet.link import TCLink
@@ -57,10 +60,26 @@ class ProjectTopology(Topo):
 # Mininet si aspetta che sia presente un dizionario `topos`
 # nello scope globale del proprio file di configurazione,
 # contenente tutte le topologie custom che si desidera utilizzare.
-topos = {
+topos: Dict[str, Topo] = {
     "test": TestTopology,
     "project": ProjectTopology,
 }
+
+@dataclass(frozen=True)
+class SwitchConfig:
+    id: int
+    addresses: List[str] = field(default_factory=list)
+
+    def yield_addresses(self):
+        yield from self.addresses
+
+
+def post_configs(endpoint: str, configs: List[SwitchConfig]):
+    for switch in configs:
+        id = switch.id
+        for address in switch.yield_addresses():
+            requests.post(url=f"{endpoint}/router/{id:016}", json={"address": address})
+
 
 def create_network():
     net = Mininet(topo=ProjectTopology(), switch=OVSKernelSwitch, link=TCLink, autoSetMacs=True, controller=RemoteController, waitConnected=True)
@@ -70,11 +89,15 @@ def create_network():
         sw = net.get(f'sw{switch_id}')
         log.info(sw.cmd(f'ovs-vsctl set Bridge sw{switch_id} protocols=OpenFlow13'))
 
-    response = requests.post(url='http://localhost:8080/router/0000000000000001', data={
-        "address": "180.0.0.1/30",
-    })
+    switches_config = [
+        SwitchConfig(id=1, addresses=["10.0.0.254/24", "180.0.0.1/30", "200.0.0.1/30"]),
+        SwitchConfig(id=2, addresses=["11.0.0.254/24", "180.0.0.2/30", "180.1.1.1/30"]),
+        SwitchConfig(id=3, addresses=["192.168.1.254/24", "200.0.0.2/30", "170.0.0.1/30"]),
+        SwitchConfig(id=4, addresses=["10.8.1.254/24", "170.0.0.2/30", "180.1.2.1/30"]),
+        SwitchConfig(id=5, addresses=["180.1.1.2/30", "180.1.2.2/30"]),
+    ]
+    post_configs(endpoint="http://localhost:8080", configs=switches_config)
 
-    print(response.content)
     CLI(net)
     net.stop()
 
