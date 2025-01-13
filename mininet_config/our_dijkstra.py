b5b64d66a429a71a2425f80359e7d3a0be52b335
Domenico Puglisi <domenico.puglisi@community.unipa.it>
2025-01-13 21:14:43 +0100

    Aggiunti commenti per migliorare la leggibilità del codice.

--- mininet_config/our_dijkstra.py
+++ mininet_config/our_dijkstra.py
@@ -15,7 +15,7 @@ from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
-@dataclass()
+@dataclass(frozen=True)
 class DistanceEntry():
     cost: int
     previous_dpid: Optional[int]
@@ -26,22 +26,19 @@ class NetLinkGraph():
         self.parent_app = parent_app
         self.switch_map: Dict[int, Switch] = { switch.dp.id: switch for switch in switches }
 
+        # Mappa (id_src, id_dst) -> Costo fra i link
         self.node_adjacency: Dict[Tuple[int, int], int] = defaultdict(lambda: 0)
         for link in links.keys():
             src: Port = link.src
             dst: Port = link.dst
 
-            # Mappa (id_src, id_dst) -> Costo fra i link
+            # I seguenti corrispondono ai PESI dei collegamenti fra switch ADIACENTI
             self.node_adjacency[src.dpid, dst.dpid] = 1
-            self.node_adjacency[dst.dpid, src.dpid] = 1 # Superfluo, no?
+            self.node_adjacency[dst.dpid, src.dpid] = 1 # Superfluo, credo
 
         # DEBUG CODE
         for (src_dpid, dst_dpid) in (self.node_adjacency.keys()):
             print(f"Lo switch {src_dpid} raggiunge {dst_dpid}")
-        # print()
-        # for src_port in set(src_port for src_port, _ in self.node_adjacency.keys()):
-            # switch = self.switch_map[src_port.dpid]
-            # print(f"La porta {src_port.hw_addr} ha switch {switch}")
 
     def dijkstra(self, starting_switch_id: int) -> Dict[int, DistanceEntry]:
         distances: Dict[int, DistanceEntry] = {
@@ -51,24 +48,26 @@ class NetLinkGraph():
             )
             for id in self.switch_map.keys()
         }
-        queue: Set[int] = set(self.switch_map.keys())
+        to_explore: Set[int] = set(self.switch_map.keys())
         
+        # Funzione helper che, identificato lo switch con id `switch_id`,
+        # restituisce tutti gli altri switch ad esso adiacente
         def yield_neighbors_of(switch_id: int):
             this_switch = self.switch_map[switch_id]
             for other_switch in self.switch_map.values():
                 if other_switch != this_switch and self.node_adjacency[this_switch.dp.id, other_switch.dp.id]:
                     yield other_switch
 
-        while queue:
-            u = min(filter(queue.__contains__, distances), key=lambda d: distances.get(d).cost)
-            queue.remove(u)
-            for v in yield_neighbors_of(u):
-                if v.dp.id not in queue: # Già visitato!
+        while to_explore:
+            current_switch_id = min(filter(to_explore.__contains__, distances), key=lambda d: distances.get(d).cost)
+            to_explore.remove(current_switch_id)
+            for neighboring_switch in yield_neighbors_of(current_switch_id):
+                if neighboring_switch.dp.id not in to_explore: # Già visitato!
                     continue
 
-                new_cost = distances[u].cost + self.node_adjacency.get((u, v.dp.id))
-                if new_cost < distances[v.dp.id].cost:
-                    distances[v.dp.id] = DistanceEntry(cost=new_cost, previous_dpid=u)
+                new_cost = distances[current_switch_id].cost + self.node_adjacency[current_switch_id, neighboring_switch.dp.id]
+                if new_cost < distances[neighboring_switch.dp.id].cost:
+                    distances[neighboring_switch.dp.id] = DistanceEntry(cost=new_cost, previous_dpid=current_switch_id)
 
         return distances
 
@@ -76,36 +75,34 @@ class NetLinkGraph():
 class DijkstraRouter(RestRouterAPI):
     _CONTEXTS = { 'wsgi': WSGIApplication }
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    EVENTS = [
-        event.EventSwitchEnter,
-        event.EventSwitchLeave, event.EventPortAdd,
-        event.EventPortDelete, event.EventPortModify,
-        event.EventLinkAdd, event.EventLinkDelete
-    ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         wsgi = kwargs['wsgi']
-        wsgi.register(DijkstraCommand, { "app": self })
+        wsgi.register(DijkstraCommand, {
+            "app": self # Necessario per consentire al controller di
+            # ricevere dati sulla topologia tramite l'API ryu.topology
+        })
 
 
 class DijkstraCommand(ControllerBase):
     def __init__(self, req, link, data, **config):
         super().__init__(req, link, data, **config)
-        self.__app = data["app"]
+        self.__app = data["app"] # Recuperiamo l'applicazione per poter
+        # fare uso dell'API ryu.topology
 
     @route(name='calc_dijkstra', path='/dijkstra', methods=['GET', 'POST'], requirements={})
     def calc_dijkstra(self, req: Request, **_kwargs) -> Response:
         switch_list: List[Switch] = get_all_switch(self.__app)
         links_dict: Dict[Link, float] = get_all_link(self.__app)
 
-        obj = NetLinkGraph(parent_app=self.__app, switches=switch_list, links=links_dict)
-        results = obj.dijkstra(starting_switch_id=switch_list[0].dp.id)
+        net_graph = NetLinkGraph(parent_app=self.__app, switches=switch_list, links=links_dict)
+        results = net_graph.dijkstra(starting_switch_id=switch_list[0].dp.id)
 
         # DEBUG
         for r, it in results.items():
-            print(f"id={r} con costo {it.cost} (Prev: id={obj.switch_map[it.previous_dpid].dp.id if it.previous_dpid else None})")        
+            print(f"id={r} con costo {it.cost} (Prev: id={net_graph.switch_map[it.previous_dpid].dp.id if it.previous_dpid else None})")        
         print()
 
         return Response(status=200)
